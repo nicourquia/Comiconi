401a99db52b922931fc11cc2fd2a4f0b3fab6ed8
Nicolás Urquía <63379409+nicourquia@users.noreply.github.com>
2022-11-23 19:18:40 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import './App.css';
+import NavBar from './components/NavBar/NavBar'
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import CartView from './components/CartView/CartView';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
+import { HashRouter, Route, Routes }  from 'react-router-dom';
+import { CartContextProvider } from './store/CartContext';
+import Footer from './components/Footer';
+import CheckOut from './components/CheckOut/';
+import Inicio from './pages/Inicio';
+import Acerca from './pages/Acerca';
+import Contacto from './pages/Contacto';
+
+
+function App() {
+  
+  return (
+    <>
+    <CartContextProvider>
+    <HashRouter>
+      <NavBar />
+      <main>
+      <Routes>
+        <Route path="/inicio" element={<Inicio />} />
+        <Route path="/acercade" element={<Acerca />} />
+        <Route path="/contacto" element={<Contacto />} />
+        <Route path="/checkout" element={<CheckOut />} />
+        <Route path="/item/:itemid" element={<ItemDetailContainer greeting="Detalle de producto"/>} />  
+        <Route path="/" element={<ItemListContainer greeting="Listado de Comics"/>} />
+        <Route path="/cart" element={<CartView greeting="Este es su carrito"/>} / >
+        <Route path="/category/:categoryid" element={<ItemListContainer greeting="Categoría"/>} />
+      </Routes>
+      <hr />
+      <div className="back1">
+<div className="back1 back2">
+<div className="back1 back3">
+			</div>
+          </div>
+         </div>
+      </main>
+      <Footer />
+      </HashRouter>
+      </CartContextProvider>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Contacto/index.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import './Contacto.css';
+import '../../assets/css/animaciones.css'
+
+function Contacto() {
+    document.title = 'Comiconi Contacto';
+  return (
+    <section className="slide-in-fwd-center">
+  <div className="container  mt-5 mb-5 ">
+    <div className="row m-0">
+      <div className="col-md-3 bg-custom p-0 d-flex text-center  mx-auto justify-content-center flex-column">
+        <h3 className="mb-2 text-white">Info</h3>
+        <div className="info text-light .fs-6">
+          Av. San Juan y Av. Boedo
+          <p>contacto@comiconi.com</p>
+          12345678
+          <div className="social-icons">
+          </div>
+        </div>
+      </div>
+      <div className="col-md-8 p-0 bg-custom2 pt-4 pb-4">
+        <form className="w-100 p-4" action="#">
+          <div className="row">
+            <div className="col-md-6">
+              <div className="mb-3">
+                <input type="text" className="form-control" id="exampleFormControlInput1" placeholder="Nombre" required />
+              </div>
+            </div>
+            <div className="col-md-6">
+              <div className="mb-3">
+                <input type="email" className="form-control" id="exampleFormControlInput2" placeholder="Email" required />
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-12">
+              <div className="mb-3">
+                <textarea className="form-control" id="exampleFormControlInput2" placeholder="Mensaje" required rows={3} defaultValue={""} />
+              </div>
+            </div>
+          </div>
+          <button type="button" className="btn btn-custom btn-lg btn-block mt-3">Enviar</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</section>
+  )
+}
+
+export default Contacto
